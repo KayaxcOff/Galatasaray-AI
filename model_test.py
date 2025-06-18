8b5b7a745300e16691cd1bcd1a02aa25b358192a
Muhammet Kaya <muham123cak@gmail.com>
2025-06-18 09:38:30 +0300

    model_test.py

--- model_test.py
+++ model_test.py
@@ -1,18 +1,20 @@
 from tensorflow.keras.models import load_model
 import pandas as pd
 import joblib
+import numpy as np
 
 model = load_model("gs_ai_model.keras")
 scaler = joblib.load("scaler.pkl")
+x_columns = joblib.load("x_columns.pkl")
 
 guessThePosition = str(input("Tell us the position the player plays: "))
 guessTheCountry = str(input("Tell us the country the player come from: "))
 guessTheAge = int(input("What is the player's age: "))
 guessTheNumber = int(input("What is the player's number: "))
 guessTheFoot = str(input("Which foot the player uses: "))
-guessTheMatch = int(input("How much match the player played: "))
-guessTheGoal = int(input("How much goal the player scored: "))
-guessTheAsist = int(input("How much goal the player helped: "))
+guessTheMatch = int(input("How many matches the player played: "))
+guessTheGoal = int(input("How many goals the player scored: "))
+guessTheAssist = int(input("How many assists the player has: "))
 
 newDataBase = pd.DataFrame([{
     "Mevki" : guessThePosition,
@@ -22,22 +24,21 @@ newDataBase = pd.DataFrame([{
     "Kullandığı ayak" : guessTheFoot,
     "Maç sayısı" : guessTheMatch,
     "Gol" : guessTheGoal,
-    "Asist" : guessTheAsist
+    "Asist" : guessTheAssist
 }])
 
 newDataBase = pd.get_dummies(newDataBase)
 
-dataBaseForGuess = pd.read_excel("guessThePlayer.xlsx")
-for col in dataBaseForGuess.columns:
+for col in x_columns:
     if col not in newDataBase.columns:
         newDataBase[col] = 0
-newDataBase = newDataBase[dataBaseForGuess.columns]
+
+newDataBase = newDataBase[x_columns]
 
 newDataBase_scaled = scaler.transform(newDataBase)
 
 prediction = model.predict(newDataBase_scaled)
 
-import numpy as np
 player_index = np.argmax(prediction)
-print("Tahmin edilen oyuncu indexi:", player_index)
-print("Olasılık dağılımı:", prediction)
+print("Index of the player:", player_index)
+print("probability distribution:", prediction)
